82364fad0022431c3adcff80f1535bd5ad218846
Nazmul Islam Talukdar <nazmul.islam.spi33.com>
2022-07-22 19:43:52 +0600

    Product

---
+++ database/migrations/2022_07_22_133717_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->uniqid();
+            $table->foreignId('category_id')->nullable();
+            $table->foreignId('subcategory_id');
+            $table->string('summery');
+            $table->text('description');
+            $table->string('price');
+            $table->string('thumbnail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
